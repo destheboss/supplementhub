5bcaf2457ad17cd3ec7449e80b7715ce14f75ea1
Desi <desislav.hristovv@gmail.com>
2025-11-19 21:24:42 +0100

    Added API Gateway + routes & Feign for asynchronous communication between services

--- services/order-service/src/main/java/dev/destheboss/microservices/order/business/OrderService.java
+++ services/order-service/src/main/java/dev/destheboss/microservices/order/business/OrderService.java
@@ -1,5 +1,6 @@
 package dev.destheboss.microservices.order.business;
 
+import dev.destheboss.microservices.order.client.InventoryClient;
 import dev.destheboss.microservices.order.dto.OrderRequest;
 import dev.destheboss.microservices.order.model.Order;
 import dev.destheboss.microservices.order.persistence.OrderRepository;
@@ -12,13 +13,20 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock.");
+        }
     }
 }
--- services/order-service/src/test/java/dev/destheboss/microservices/order/integration/OrderApiIntegrationTests.java
+++ services/order-service/src/test/java/dev/destheboss/microservices/order/integration/OrderApiIntegrationTests.java
@@ -1,5 +1,7 @@
 package dev.destheboss.microservices.order.integration;
 
+import dev.destheboss.microservices.order.client.InventoryClient;
+import dev.destheboss.microservices.order.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -17,6 +20,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 @Testcontainers(disabledWithoutDocker = true)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@AutoConfigureWireMock(port = 0)
 class OrderApiIntegrationTests {
 
     @ServiceConnection
@@ -41,6 +45,7 @@ class OrderApiIntegrationTests {
                     "quantity": 1
                 }
                 """;
+        InventoryClientStub.stubInventoryCall("iphone_15", 1);
 
         var responseBodyString = RestAssured.given()
                 .contentType("application/json")
